aa64b2429a5360c858b3d682a6a83bba1bc6e876
omerm <omermadmon@gmail.com>
2021-08-29 18:49:09 +0300

    Implement merge inp files, split output files to replace and merge files.

--- inp_transformer.py
+++ inp_transformer.py
@@ -3,7 +3,8 @@ from collections import OrderedDict
 
 INPUT_PATH = 'inp/original.inp'
 FORMAT_PATH = 'inp/format.inp'
-OUTPUT_PATH = 'inp/output.inp'
+REPLACE_OUTPUT_PATH = 'inp/replace_output.inp'
+MERGE_OUTPUT_PATH = 'inp/merge_output.inp'
 
 block_to_take_from_format = [
     '[OPTIONS]',
@@ -50,7 +51,8 @@ def split_inp_to_blocks(path):
     return res
 
 
-def replace_blocks(output_path, original_file_path, alternative_file_path, block_to_remove, block_to_add):
+def replace_blocks(output_path, original_file_path, alternative_file_path,
+                   block_to_remove, block_to_add):
     # Load original and alternative files:
     original_lines = extract_lines(original_file_path)
     alternative_lines = extract_lines(alternative_file_path)
@@ -75,12 +77,48 @@ def replace_blocks(output_path, original_file_path, alternative_file_path, block
             f.write('\n')
 
 
+def merge_files(path_1, path_2, blocks_order, blocks_from_1, blocks_from_2, output_path):
+    # Load files:
+    lines_1 = extract_lines(path_1)
+    lines_2 = extract_lines(path_2)
+
+    # Split to blocks:
+    blocks_of_1 = split_inp_to_blocks(path_1)
+    blocks_of_2 = split_inp_to_blocks(path_2)
+
+    # Write output file:
+    with open(output_path, "w") as f:
+        for block in blocks_order:
+            if block in blocks_from_1:
+                beg, end = blocks_of_1[block]
+                for i in range(beg, end):
+                    f.write(lines_1[i])
+                f.write('\n')
+            elif block in blocks_from_2:
+                beg, end = blocks_of_2[block]
+                for i in range(beg, end):
+                    f.write(lines_2[i])
+                f.write('\n')
+            else:
+                continue
+
+
 def main():
-    replace_blocks(output_path=OUTPUT_PATH,
+    block_titles_union = set(split_inp_to_blocks(INPUT_PATH))\
+        .union(set(split_inp_to_blocks(FORMAT_PATH)))
+
+    replace_blocks(output_path=REPLACE_OUTPUT_PATH,
                    original_file_path=INPUT_PATH,
                    alternative_file_path=FORMAT_PATH,
                    block_to_remove='[INFLOWS]',
                    block_to_add='[POLLUTANTS]')
+
+    merge_files(path_1=INPUT_PATH,
+                path_2=FORMAT_PATH,
+                blocks_order=block_titles_union,
+                blocks_from_1=block_to_take_from_original,
+                blocks_from_2=block_to_take_from_format,
+                output_path=MERGE_OUTPUT_PATH)
 
 
 if __name__ == '__main__':
